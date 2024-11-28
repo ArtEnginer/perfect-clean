ec0d46d62d21b654f0200f6a2574509179439f21
ANGGA <anggasap2210@gmail.com>
2024-11-28 15:52:00 +0700

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Atur jika Anda menggunakan file HTML di root proyek
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Sesuaikan dengan struktur proyek Anda
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
